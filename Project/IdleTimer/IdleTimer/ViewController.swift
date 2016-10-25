774841b6fc69638d47069d8adddb110bb6317bca
한상훈 <shhan826@gmail.com>
2016-10-25 11:01:44 +0900

    Setting

---
+++ Project/IdleTimer/IdleTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdleTimer
+//
+//  Created by 한상훈 on 2016. 10. 25..
+//  Copyright © 2016년 Sanghoon Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
