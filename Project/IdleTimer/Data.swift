a5481f37954ab71e7df67e118f17eadb4400494a
한상훈 <shhan826@gmail.com>
2016-11-01 11:31:52 +0900

    이슈 커멘트 연습 #1

---
+++ Project/IdleTimer/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  IdleTimer
+//
+//  Created by 한상훈 on 2016. 11. 1..
+//  Copyright © 2016년 Sanghoon Han. All rights reserved.
+//
+
+import Foundation
+
+let dataCenter : Data = Data()
+
+class Data {
+    var history : [[Date]] = []
+    var resource = [0, 1]
+}
