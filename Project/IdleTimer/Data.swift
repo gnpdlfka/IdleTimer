553957987f110994f3dfa72180c0b504bf67422a
한상훈 <shhan826@gmail.com>
2016-12-13 05:56:15 +0900

    데이터 저장
    구현 못함. 에러 떠서 다시 지

--- Project/IdleTimer/Data.swift
+++ Project/IdleTimer/Data.swift
@@ -12,61 +12,69 @@ import Foundation
 
 let dataCenter : dataClass = dataClass()
 let dateFormatter = DateFormatter()
+//let fileName = "BranchData.brch"
 
-class dataClass: NSObject, NSCoding {
+class dataClass{
+//    var filePath : String  { get{
+//        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+//        return documentDirectory + fileName
+//        }}
+    
     var count:Int = 0
     var history : [historyClass]
     var coin:Int = 30
     var category = ["Default", "Health", "Study", "Me", "Friend"]
     var item:[Bool] = [false, true, true, true, true, true]
-
-    override init() {
-        history = [historyClass()]
+    
+    init() {
         dateFormatter.dateFormat = "yyyy년 M월 d일"
+        history = [historyClass()]
+
+//        if (FileManager.default.fileExists(atPath: self.filePath)){
+//            if let unarchArray = NSKeyedUnarchiver.unarchiveObject(withFile: self.filePath) as? [historyClass] {
+//                history += unarchArray
+//            }
+//        }else{
+//            history = [historyClass()]
+//        }
     }
-    required init?(coder aDecoder: NSCoder) {
-        self.count = aDecoder.decodeObject(forKey: "count") as! Int
-        self.history = aDecoder.decodeObject(forKey: "history") as! [historyClass]
-        self.coin = aDecoder.decodeObject(forKey: "coin") as! Int
-        self.item = aDecoder.decodeObject(forKey: "item") as! [Bool]
-    }
-    func encode(with aCoder: NSCoder) {
-        aCoder.encodeConditionalObject(self.count, forKey: "count")
-        aCoder.encodeConditionalObject(self.history, forKey: "history")
-        aCoder.encodeConditionalObject(self.coin, forKey: "coin")
-        aCoder.encodeConditionalObject(self.item, forKey: "item")
-    }
+    
+    
     var results:Dictionary<String,Int> = ["success":0, "fail":0]
+    
+//    func save(){
+//        NSKeyedArchiver.archiveRootObject(self.history, toFile: self.filePath)
+//    }
 }
 
-class historyClass: NSObject, NSCoding {
+class historyClass{
     var seconds : Int
     var title : String
     var success : Bool
     var date = NSDate()
     var category : String
     
-    override init(){
+    init(){
         seconds = 0
         title = "Sample"
         success = false
         date = NSDate()
         category = "Default"
     }
-    required init?(coder aDecoder: NSCoder) {
-        self.seconds = aDecoder.decodeObject(forKey: "seconds") as! Int
-        self.title = aDecoder.decodeObject(forKey: "title") as! String
-        self.success = aDecoder.decodeObject(forKey: "success") as! Bool
-        self.date = aDecoder.decodeObject(forKey: "date") as! NSDate
-        self.category = aDecoder.decodeObject(forKey: "category") as! String
-    }
-    func encode(with aCoder: NSCoder) {
-        aCoder.encodeConditionalObject(self.seconds, forKey: "seconds")
-        aCoder.encodeConditionalObject(self.title, forKey: "title")
-        aCoder.encodeConditionalObject(self.success, forKey: "success")
-        aCoder.encodeConditionalObject(self.date, forKey: "date")
-        aCoder.encodeConditionalObject(self.category, forKey: "category")
-    }
+//    required init?(coder aDecoder: NSCoder) {
+//        self.seconds = aDecoder.decodeObject(forKey: "seconds") as! Int
+//        self.title = aDecoder.decodeObject(forKey: "title") as! String
+//        self.success = aDecoder.decodeObject(forKey: "success") as! Bool
+//        self.date = aDecoder.decodeObject(forKey: "date") as! NSDate
+//        self.category = aDecoder.decodeObject(forKey: "category") as! String
+//    }
+//    func encode(with aCoder: NSCoder) {
+//        aCoder.encodeConditionalObject(self.seconds, forKey: "seconds")
+//        aCoder.encodeConditionalObject(self.title, forKey: "title")
+//        aCoder.encodeConditionalObject(self.success, forKey: "success")
+//        aCoder.encodeConditionalObject(self.date, forKey: "date")
+//        aCoder.encodeConditionalObject(self.category, forKey: "category")
+//    }
 }
 
 //StartVewConroller, DoingViewController에 쓰이는 데이터입니다.
